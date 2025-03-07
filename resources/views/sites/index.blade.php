545defde574ffdccd8933edd798af006f50ce275
Satheesh Kumar <satheeshssk@icloud.com>
2025-03-07 12:55:09 +0530

    Update index.blade.php

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="10">
     <title>Monitored Sites</title>
     @vite('resources/css/app.css')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.7/dayjs.min.js"></script>
